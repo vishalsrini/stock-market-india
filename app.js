0c12323a1807927e84cfcdca0ebbd433e7850949
Vishal Srinivasan <vishalvishal619@gmail.com>
2020-11-23 01:16:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var NSEAPI = API.NSE;
 
 var app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
 
 // National Stock Exchange (NSE) APIS
